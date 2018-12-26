d09c31635bef9f5eb69d8393fc7ac7dd695de469
dpkcse <dpkcse@users.noreply.github.com>
2018-12-27 00:17:34 +0600

    update

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../config/db');
-
-router.get('/', function(req,res,next){
-    res.render('dashboard');
-});
-
-module.exports = router;

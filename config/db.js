157b9edca9d4c5e877e114b3136b86a15f3382d0
Dipok Chakraborty <dipokchakraborty@gmail.com>
2018-12-26 17:20:42 +0600

    Update

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     // host     : '85.10.205.173',
-//     host     : 'db4free.net',
-//     user     : 'hyper_admin123',
-//     password : 'ZhXHxf9ZZQgHdir',
-//     database : 'hyper_mms007'
-// });
-
 var connection = mysql.createConnection({
-    host     : '172.16.0.64',
-    user     : 'root',
-    password : 'YeezY@001',
-    database : 'taskmanager'
+    // host     : '85.10.205.173',
+    host     : 'db4free.net',
+    user     : 'hyper_admin123',
+    password : 'ZhXHxf9ZZQgHdir',
+    database : 'hyper_mms007'
 });
+
+// var connection = mysql.createConnection({
+//     host     : '172.16.0.64',
+//     user     : 'root',
+//     password : 'YeezY@001',
+//     database : 'taskmanager'
+// });
 
 connection.connect(function(err) {
     if (err) throw err;
